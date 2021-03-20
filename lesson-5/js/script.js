d67d108d090ff4bbc7c4bd61457eb9797bf913fc
Md Abdullah Al Mashud <almashud137@gmail.com>
2021-03-20 22:11:33 +0600

    0005. lesson-5 string data type

---
+++ lesson-5/js/script.js
@@ -0,0 +1,22 @@
+let name = "maddi"
+console.log(name)
+
+let firstName = "al"
+let lastName = "mashud"
+
+let fullName = firstName + " " + lastName
+
+console.log(fullName)
+
+console.log(firstName[1])
+console.log(lastName[4])
+
+console.log(fullName.length)
+
+let address = "New York"
+
+console.log(address.toUpperCase())
+
+console.log(address)
+
+console.log(address.toLowerCase())
